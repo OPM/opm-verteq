44499770b605c6b2548a9b5cc3c76970c70a98ae
Roland Kaufmann <roland.kaufmann@uni.no>
2012-11-14 14:30:54 +0100

    Main file skeleton

---
+++ examples/3d/src/co2_3d.cpp
@@ -0,0 +1,5 @@
+/* -*- mode: c++; tab-width: 2; indent-tabs-mode: t; truncate-lines: t -*- */
+/* vim: set filetype=cpp autoindent tabstop=2 shiftwidth=2 noexpandtab softtabstop=2 nowrap: */
+int main (int argc, char *argv[]) {
+	return 0;
+}
