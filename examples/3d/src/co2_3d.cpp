76b646192518230f2e2d37fef3d88b0176e6d026
Roland Kaufmann <roland.kaufmann@uni.no>
2012-11-15 10:01:03 +0100

    Read parameters from command-line

--- examples/3d/src/co2_3d.cpp
+++ examples/3d/src/co2_3d.cpp
@@ -1,5 +1,22 @@
 /* -*- mode: c++; tab-width: 2; indent-tabs-mode: t; truncate-lines: t -*- */
 /* vim: set filetype=cpp autoindent tabstop=2 shiftwidth=2 noexpandtab softtabstop=2 nowrap: */
+#include <opm/core/utility/parameters/ParameterGroup.hpp>
+
+#include <iostream>
+
+using namespace Opm::parameter;
+using namespace std;
+
 int main (int argc, char *argv[]) {
+	// read parameters from command-line
+	ParameterGroup param (argc, argv, false);
+
+	// if some parameters were unused, it may be that they're spelled wrong
+	if (param.anyUnused ()) {
+		cerr << "Unused parameters:" << endl;
+		param.displayUsage ();
+	}
+
+	// done
 	return 0;
 }
