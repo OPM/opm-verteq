bf311d9caff7e45e55233c59b94bdbad9f98c2f2
Roland Kaufmann <roland.kaufmann@uni.no>
2013-03-12 12:13:08 +0100

    Make symbols hidden by default
    This gives better encapsulation -- client cannot use symbols that aren't
    explicitly marked as public -- and improve link time between modules.

---
+++ opm/core/utility/visibility.h
@@ -0,0 +1,38 @@
+#ifndef OPM_CORE_VISIBILITY_INCLUDED
+#define OPM_CORE_VISIBILITY_INCLUDED
+
+/**
+ * Macros to encapsulate symbol visibility on various platforms.
+ * You need to define a separate macro for your package; symbols
+ * that are exported in one dynamic shared object, may of course
+ * be imported by another one!
+ *
+ * Usage:
+ *
+ * #include <opm/core/utility/visibility.h>
+ *
+ * #if defined (foo_EXPORTS)
+ * #  define FOO_PUBLIC SYMBOL_IS_EXPORTED
+ * #else
+ * #  define FOO_PUBLIC SYMBOL_IS_IMPORTED
+ * #endif
+ * #define FOO_PRIVATE SYMBOL_IS_LOCALDEF
+ *
+ * struct FOO_PUBLIC Bar {
+ * };
+ *
+ * int FOO_PRIVATE mumble ();
+ */
+#if defined (_WIN32)
+#  define SYMBOL_IS_EXPORTED __declspec (dllexport)
+#  define SYMBOL_IS_IMPORTED __declspec (dllimport)
+#  define SYMBOL_IS_LOCALDEF
+#else
+#  if __GNUC__ >= 4
+#    define SYMBOL_IS_EXPORTED __attribute__ ((visibility ("default")))
+#    define SYMBOL_IS_IMPORTED __attribute__ ((visibility ("hidden")))
+#    define SYMBOL_IS_LOCALDEF __attribute__ ((visibility ("hidden")))
+#  endif
+#endif
+
+#endif /* OPM_CORE_VISIBILITY_INCLUDED */
