859c88df621687071cdabfc97c09a5aa32cee549
Roland Kaufmann <roland.kaufmann@uni.no>
2013-03-12 12:40:06 +0100

    Modify code necessary to export symbols

--- opm/verteq/verteq.hpp
+++ opm/verteq/verteq.hpp
@@ -1,4 +1,14 @@
 #ifndef OPM_VERTEQ_INCLUDED
 #define OPM_VERTEQ_INCLUDED
 
+#include <opm/core/utility/visibility.h>
+
+#if defined(opmverteq_EXPORTS)
+#  define OPM_VERTEQ_PUBLIC  SYMBOL_IS_EXPORTED
+#else
+#  define OPM_VERTEQ_PUBLIC  SYMBOL_IS_IMPORTED
+#endif
+#define OPM_VERTEQ_PRIVATE  SYMBOL_IS_LOCALDEF
+
+
 #endif /* OPM_VERTEQ_INCLUDED */
