463567304fc98b3d9f0738860df50d91240d79e8
Roland Kaufmann <roland.kaufmann@uni.no>
2014-05-28 09:28:59 +0200

    Convert upscaled example to new parser also

--- examples/2d/src/co2_2d.cpp
+++ examples/2d/src/co2_2d.cpp
@@ -1,7 +1,6 @@
 /* -*- mode: c++; tab-width: 2; indent-tabs-mode: t; truncate-lines: t -*- */
 /* vim: set filetype=cpp autoindent tabstop=2 shiftwidth=2 noexpandtab softtabstop=2 nowrap: */
 #include <opm/core/utility/parameters/ParameterGroup.hpp>
-#include <opm/core/io/eclipse/EclipseGridParser.hpp>
 #include <opm/core/grid/GridManager.hpp>
 #include <opm/core/props/IncompPropertiesFromDeck.hpp>
 #include <opm/core/simulator/initState.hpp>
@@ -14,9 +13,12 @@
 #include <opm/core/simulator/SimulatorIncompTwophase.hpp>
 #include <opm/core/simulator/SimulatorReport.hpp>
 #include <opm/core/simulator/SimulatorOutput.hpp>
+#include <opm/parser/eclipse/Parser/Parser.hpp>
+#include <opm/parser/eclipse/EclipseState/EclipseState.hpp>
 #include <opm/verteq/wrapper.hpp>
 
 #include <iostream>
+#include <memory>
 #include <vector>
 
 using namespace Opm;
@@ -31,7 +33,8 @@ int main (int argc, char *argv[]) try {
 	// TODO: requirement that file exists
 	const string filename = param.get <string> ("deck_filename");
 	cout << "Reading deck: " << filename << endl;
-	const EclipseGridParser parser (filename);
+	const Parser parserGenerator;
+	auto parser = parserGenerator.parseFile (filename);
 
 	// extract grid from the parse tree
 	const GridManager gridMan (parser);
@@ -46,7 +49,9 @@ int main (int argc, char *argv[]) try {
 	initStateFromDeck (grid, fluid, parser, gravity [2], state);
 
 	// setup wells from input, using grid and rock properties read earlier
-	WellsManager wells (parser, grid, fluid.permeability());
+	auto eclipseState = make_shared <EclipseState> (parser);
+	int reportStepIdx = 0;
+	WellsManager wells (eclipseState, reportStepIdx, grid, fluid.permeability());
 	WellState wellState; wellState.init (wells.c_wells(), state);
 
 	// no sources and no-flow boundary conditions
@@ -55,7 +60,8 @@ int main (int argc, char *argv[]) try {
 
 	// run schedule
 	SimulatorTimer stepping;
-	stepping.init (parser);
+	auto timeMap = make_shared <TimeMap> (parser);
+	stepping.init (timeMap);
 
 	// pressure and transport solvers
 	LinearSolverFactory linsolver (param);
@@ -64,7 +70,7 @@ int main (int argc, char *argv[]) try {
 
 	// write the state at all reporting times
 	SimulatorOutput <VertEqWrapper <SimulatorIncompTwophase> > outp (
-		param, parser, grid, stepping, state, wellState, sim); (void) outp;
+		param, *parser, *timeMap, grid, stepping, state, wellState, sim); (void) outp;
 
 	// if some parameters were unused, it may be that they're spelled wrong
 	if (param.anyUnused ()) {
